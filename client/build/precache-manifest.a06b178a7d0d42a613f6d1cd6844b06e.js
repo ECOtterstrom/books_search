13a3ba0957a3d22a8899361d78a5f920a0faa901
Erin Otterstrom <hapiotter@gmail.com>
2020-09-01 20:46:01 -0600

    ran build process

---
+++ client/build/precache-manifest.a06b178a7d0d42a613f6d1cd6844b06e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8089fbc223f09c16853bed1808c49bd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "271bcac4cc852b15d7dc",
+    "url": "/static/css/main.18217b86.chunk.css"
+  },
+  {
+    "revision": "53a921fa35cdee509f4d",
+    "url": "/static/js/2.9a8586ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9a8586ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "271bcac4cc852b15d7dc",
+    "url": "/static/js/main.73fa967f.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
